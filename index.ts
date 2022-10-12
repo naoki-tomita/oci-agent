e53c6a954ef7b399f77e8b75ef79897eba772bde
naoki.tomita <naoki.tomita@uzabase.com>
2022-10-12 21:41:49 +0900

    improve file

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { serve } from "https://deno.land/std@0.159.0/http/server.ts";
 
 const decoder = new TextDecoder();
 async function $(command: string) {
+  console.log(command);
   const p = Deno.run({ cmd: ["bash", "-c", command], stdout: "piped", stderr: "piped" });
   const [status, output, stdErrOutput] = await Promise.all([
     p.status(), p.output(), p.stderrOutput()
@@ -11,9 +12,12 @@ async function $(command: string) {
   console.error(decoder.decode(stdErrOutput));
 }
 
-
-function main() {
-  const { ACCESS_TOKEN = "DUMMY" } = config();
+async function main() {
+  const { ACCESS_TOKEN = "DUMMY" } = await Deno.readTextFile(`${Deno.env.get("HOME")}/.oci-config`)
+    .then(it => it.trim())
+    .then(it => it.split("\n"))
+    .then(lines => lines.map(line => line.split("=")))
+    .then(lines => lines.reduce((prev, [key, value]) => ({ ...prev, [key]: value }), {} as Record<string, string>))
   serve(async (req) => {
     const token = req.headers.get("authorization")?.replace("Bearer", "").trim() ?? "";
     if (ACCESS_TOKEN === token) {
@@ -21,7 +25,7 @@ function main() {
       await $("docker compose up -d");
       return new Response("{}", { status: 200 });
     }
-    return new Response("No access right.", { status: 403 });
+    return new Response(`No access right. "${token}"`, { status: 403 });
   });
 }
 
