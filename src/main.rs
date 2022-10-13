6fea10bb71bda39fe7858185d46929d35eb87ac5
naoki.tomita <naoki.tomita@uzabase.com>
2022-10-13 20:23:27 +0900

    Agent can access from outer world.

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ async fn main() {
 
   ACCESS_TOKEN.set(readed.trim().into()).unwrap();
 
-  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
   let make_svc = make_service_fn(|_conn| async {
     Ok::<_, Infallible>(service_fn(command))
   });
