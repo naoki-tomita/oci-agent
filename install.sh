1b827f60c4f36b0e1febbb99835c59822998625c
naoki.tomita <naoki.tomita@uzabase.com>
2022-10-13 19:36:32 +0900

    improve script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-curl https://github.com/naoki-tomita/oci-agent/releases/download/main/aarch64-binary -o oci-agent
+curl https://github.com/naoki-tomita/oci-agent/releases/download/main/aarch64-binary -L -o oci-agent
 chmod +x oci-agent
 sudo mv oci-agent /usr/local/bin
+touch ~/.oci-config
